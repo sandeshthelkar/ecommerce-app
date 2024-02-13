4abfc772e236b040d273f45e68707892a581bf4e
Sandesh Thelkar <sandeshthelkar@gmail.com>
2024-02-13 19:42:12 +0530

    1

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -1,4 +1,4 @@
-package com.luv2code.ecommerce.entity.dao;
+package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,4 +1,4 @@
-package com.luv2code.ecommerce.entity.dao;
+package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
